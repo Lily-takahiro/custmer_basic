4159789c1224dc0f3d29f56016a7c7c9cf6146ab
Lily-takahiro <tttsss120604280520@gmail.com>
2025-08-28 03:45:58 +0900

    基礎編終了 不安あり

--- basic.py
+++ basic.py
@@ -1,49 +1,41 @@
-# CRM Application
-
-
-def show_menu():
-    print("===== Welcome to CRM Application =====")
-    print("[S]how: Show all users info")
-    print("[A]dd: Add new user")
-    print("[Q]uit: Quit The Application")
-    print("======================================")
-
-
-def show_users(users):
-    if not users:
-        print("No users found.")
-    else:
-        for user in users:
-            print(f"Name: {user['name']} Age: {user['age']}")
-
-
-def add_user(users):
-    name = input("New user name > ").strip()
-    age_input = input("New user age > ").strip()
-    if not age_input.isdigit():
-        print("Invalid age. Please enter a number.")
-        return
-    age = int(age_input)
-    users.append({"name": name, "age": age})
-    print(f"Add new user: {name}")
+from config import User
 
 
 def main():
-    users = [{"name": "Bob", "age": 15}, {"name": "Tom", "age": 57}, {"name": "Ken", "age": 73}]
+    print(
+        """===== Welcome to CRM Application =====
+[S]how: Show all users info
+[A]dd: Add new user
+[Q]uit: Quit application
+======================================"""
+    )
 
     while True:
-        show_menu()
-        command = input("Your command > ").strip().upper()
-
-        if command == "S":
-            show_users(users)
-        elif command == "A":
-            add_user(users)
-        elif command == "Q":
+        choice = input("Your command > ")
+        cmd = choice.upper()
+        if cmd == "S":
+            show()
+        elif cmd == "A":
+            add()
+        elif cmd == "Q":
             print("Bye!")
-            break
+            exit()
         else:
-            print(f"{command.lower()}: command not found")
+            print(f"{choice}: command not found")
+        print("")
+
+
+def show():
+    for user in User.select():
+        print(f"Name: {user.name} Age: {user.age}")
+
+
+def add():
+    name = input("New user name > ")
+    age = input("New user age > ")
+
+    user = User.create(name=name, age=age)
+    print(f"Add new user: {user.name}")
 
 
 if __name__ == "__main__":
